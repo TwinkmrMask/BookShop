46466f641446d862806381017a2c3bd62b817109
mrMask <rebootroot13@gmail.com>
2021-04-19 01:41:33 +0300

    Task completed

---
+++ DataBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Xml;
+
+namespace task_14._04
+{
+    class DataBase : IDisposable
+    {
+        public void Dispose() { }
+
+        public List<Books> GetData()
+        {
+            List<Books> books = new List<Books>();
+
+            XmlDocument document = new XmlDocument();
+            document.Load(getPath());
+            XmlElement root = document.DocumentElement;
+            MessageBox.Show(root.Name);
+            foreach (XmlElement book in root)
+            {
+                if ((root != null) && (root.HasChildNodes))
+                {
+                    books.Add(new Books
+                    {
+                        id = int.Parse(book.Attributes["id"].Value),
+                        author = book.Attributes["author"].Value,
+                        title = book.Attributes["title"].Value,
+                        year = int.Parse(book.Attributes["year"].Value)
+                    });
+                }
+            }
+
+            return books;
+        }
+        private string getPath()
+        {
+            Microsoft.Win32.OpenFileDialog openDialog = new Microsoft.Win32.OpenFileDialog();
+            openDialog.Filter = "Файл с книгами|*.xml";
+            openDialog.ShowDialog();
+            return openDialog.FileName;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Linq;
+namespace task_14._04
+{
+    public partial class MainWindow
+    {
+
+        List<Books> books;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            using(DataBase data =  new DataBase())
+                books = data.GetData();
+            insert(ref books);
+        }
+
+        void insert(ref List<Books> books) => ListOfBooks.ItemsSource = books.OrderBy(x => x.id);
+        private void delete(ref List<Books> books, Books book)
+        {
+            books.Remove(book);
+            ListOfBooks.ItemsSource = books;
+            ListOfBooks.Items.Refresh();
+        }
+        private void buy_Click(object sender, RoutedEventArgs e)
+        {
+            Books book = ListOfBooks.SelectedItem as Books;
+            delete(ref books, book);
+        }
+
+        //selecting a key for sorting
+        private void id_Selected(object sender, RoutedEventArgs e) => ListOfBooks.ItemsSource = books.OrderBy(x => x.id);
+        private void author_Selected(object sender, RoutedEventArgs e) => ListOfBooks.ItemsSource = books.OrderBy(x => x.author);
+        private void title_Selected(object sender, RoutedEventArgs e) => ListOfBooks.ItemsSource = books.OrderBy(x => x.title);
+        private void year_Selected(object sender, RoutedEventArgs e) => ListOfBooks.ItemsSource = books.OrderBy(x => x.year);
+    }
+    class Books
+    {
+        public int id { get; set; }
+        public string author { get; set; }  
+        public string title { get; set; }
+        public int year { get; set; }
+    }
+}
